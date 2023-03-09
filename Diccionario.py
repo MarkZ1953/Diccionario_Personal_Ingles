1001e19debaec6ef24fa27eb81bad071e4bf5c8c
Felipe Castro <castrolopez.davidfelipe@gmail.com>
2023-03-09 11:08:16 -0500

    .

--- Diccionario.py
+++ Diccionario.py
@@ -1,10 +1,25 @@
 from Conexion import Conexion
 
-
 class Diccionario:
     
     _SELECCIONAR = "SELECT * FROM diccionario ORDER BY id_palabra"
+    _INSERTAR = 'INSERT INTO diccionario(p_ingles, p_espanol, nota) VALUES(%s, %s, %s)'
 
     @classmethod
     def seleccionar(cls):
-        pass
+        with Conexion.obtenerConexion() as conexion:
+            with Conexion.obtenerCursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                for registro in registros:
+                    print(registro)
+    
+    @classmethod
+    def insertar(cls):
+        with Conexion.obtenerConexion() as conexion:
+            with Conexion.obtenerCursor() as cursor:
+                valores = ("Hello","Hola","Saludar a otra persona")
+                cursor.execute(cls._INSERTAR,valores)
+    
+# Diccionario().seleccionar()
+Diccionario().insertar()
---
+++ Interfaz.py
@@ -0,0 +1,9 @@
+from customtkinter import CTk,CTkEntry
+
+class Layout:
+    def __init__(self) -> None:
+        self.root = CTk()
+
+        self.root.mainloop()
+
+Layout()

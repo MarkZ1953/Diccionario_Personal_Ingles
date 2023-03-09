11f0ef43b4208c12600c4db988c187dc4185d377
Felipe Castro <castrolopez.davidfelipe@gmail.com>
2023-03-09 10:01:08 -0500

    .

---
+++ Conexion.py
@@ -0,0 +1,37 @@
+import psycopg2 as bd
+import sys
+
+class Conexion:
+    _DATABASE = 'diccionario_ingles'
+    _USERNAME = 'postgres'
+    _PASSWORD = 'admin'
+    _DB_PORT = '5432'
+    _HOST = '127.0.0.1'
+    _conexion = None
+    _cursor = None
+
+    @classmethod
+    def obtenerConexion(cls):
+        if cls._conexion is None:
+            try:
+                cls._conexion = bd.connect(host=cls._HOST,
+                                           user=cls._USERNAME,
+                                           password=cls._PASSWORD,
+                                           port=cls._DB_PORT,
+                                           database=cls._DATABASE)
+                return cls._conexion
+            except Exception as e:
+                sys.exit()
+        else:
+            return cls._conexion
+
+    @classmethod
+    def obtenerCursor(cls):
+        if cls._cursor is None:
+            try:
+                cls._cursor = cls.obtenerConexion().cursor()
+                return cls._cursor
+            except Exception as e:
+                sys.exit()
+        else:
+            return cls._cursor
---
+++ Diccionario.py
@@ -0,0 +1,10 @@
+from Conexion import Conexion
+
+
+class Diccionario:
+    
+    _SELECCIONAR = "SELECT * FROM diccionario ORDER BY id_palabra"
+
+    @classmethod
+    def seleccionar(cls):
+        pass

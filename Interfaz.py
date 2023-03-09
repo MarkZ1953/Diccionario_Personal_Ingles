f7c81ead61e2acb217f3c8b6e8b11f4ed58d7c82
Felipe Castro <castrolopez.davidfelipe@gmail.com>
2023-03-09 11:27:54 -0500

    .

--- Interfaz.py
+++ Interfaz.py
@@ -1,4 +1,4 @@
-from customtkinter import CTk,CTkEntry,CTkFrame
+from customtkinter import CTk,CTkEntry,CTkFrame,CTkTabview
 from Utiles.Centrar_Ventana_Geometry import centerwindows
 
 class Layout:
@@ -9,12 +9,22 @@ class Layout:
         self.root.columnconfigure(0,weight=1)
         self.root.rowconfigure(0,weight=1)
         self.root.config(border=20)
+        self.tabs()
         self.principal()
 
         self.root.mainloop()
     
+    def tabs(self):
+        self.tab_principal = CTkTabview(self.root)
+        self.tab_principal.grid(row=0,column=0,sticky="news")
+
+        tab1 = self.tab_principal.add("Words")
+        tab2 = self.tab_principal.add("Phrases")
+        tab3 = self.tab_principal.add("Topics")
+        tab4 = self.tab_principal.add("Settings")
+    
     def principal(self):
-        frame = CTkFrame(self.root)
+        frame = CTkFrame(self.tab_principal)
         frame.grid(row=0,column=0,sticky="news")
 
     def xd(self):

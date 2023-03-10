cd94e5e5091b0556a2de162df3bc831c039d9312
Felipe Castro <castrolopez.davidfelipe@gmail.com>
2023-03-10 16:41:26 -0500

    .

--- Tabs/Palabras.py
+++ Tabs/Palabras.py
@@ -2,9 +2,16 @@ from customtkinter import CTkFrame, CTkEntry, CTkLabel, CTk, CTkToplevel, CTkBut
 from Utiles.Centrar_Ventana_Geometry import centerwindows
 from tkinter.ttk import Treeview
 
+#Se declara una varible la cual el proposito de ella sera mantener el control 
+#de la cantidad de formularios que se puedan abrir al momento de querer agregar una nueva palabra.
 formulario_abierto = False
 
 class Palabras:
+    """
+    Clase encargada de simular un tab que es abierto desde el modulo principal,
+    en la clase se crean funciones y metodos de control para abrir y cerrar formularios, agregar 
+    nueva palabras y definiciones a la base de datos y ver informacion en las tablas.
+    """
     def __init__(self, tab_frases) -> None:
         self.tab_frases = tab_frases
         self.widgets()
@@ -17,8 +24,8 @@ class Palabras:
         global formulario_abierto
         if not formulario_abierto:
             self.temp = CTkToplevel()
-            self.temp.config(border=10)
-            centerwindows(self.temp,260,200)
+            self.temp.config(border=20)
+            centerwindows(self.temp,260,160)
             self.temp.rowconfigure(0,weight=1)
             self.temp.columnconfigure(0,weight=1)
             self.formulario()
@@ -27,17 +34,28 @@ class Palabras:
             self.temp.focus()
 
     def cerrar_formulario(self):
+        """
+        - Se ejecuta esta funcion cuando el boton de cerrar del formulario es presionado.
+        - Cambia el valor de True a False de la variable que hace que no se pueda abrir mas de una vez el formulario.
+        - Por ultimo se destruye el formulario.
+        """
         global formulario_abierto
         formulario_abierto = False
         self.temp.destroy()
 
     def formulario(self):
+        #Frame que contiene los widgets de las cajas de texto y los botones para cerrar y abrir el formulario.
         frame = CTkFrame(self.temp)
         frame.grid(row=0, column=0,sticky="news")
         frame.columnconfigure(0,weight=1)
+
         pingles = CTkEntry(frame, placeholder_text="Enlglish Word")
         pingles.grid(row=0, column=0,pady=10,padx=10,sticky="we")
+
         pespanol = CTkEntry(frame, placeholder_text="Spanish Translate")
         pespanol.grid(row=1, column=0,pady=5,padx=10,sticky="we")
+
         boton_cerrar = CTkButton(frame, text="Cerrar", command=lambda: self.cerrar_formulario())
         boton_cerrar.grid(row=2, column=0,pady=5)
+
+help(Palabras)
